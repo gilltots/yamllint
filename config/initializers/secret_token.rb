da86f2fe22ea69c2dc1cef0dd126e99b701dba61
Chris Gill <gill@new-host.home>
2010-08-30 22:17:21 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yamllint::Application.config.secret_token = '610c6187b872fecb48a7bd8183c90f1a5f3aa84371b8304826ed2fa2e4588321e735023c19498d3e75dbcc7a97fcca06338ee4c5b25286a5180a58e8c0751ff4'
